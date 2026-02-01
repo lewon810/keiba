6fb3b1955fb260997ac1527f7cfc61c949ba989f
Masatoki Mukai <lewon810@gmail.com>
2026-02-01 13:33:56 +0900

    間隔を下げる

--- train/scraper_horse.py
+++ train/scraper_horse.py
@@ -13,7 +13,7 @@ def fetch_html(url):
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
     }
     try:
-        time.sleep(1 + random.random()) # Be polite
+        time.sleep(0.5 + random.random()) # Be polite
         response = requests.get(url, headers=headers, timeout=10)
         response.encoding = response.apparent_encoding
         return response.text
